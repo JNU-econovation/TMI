ec70b65ba99857156ab8cbc59904fae4b2a89fbd
mywnajsldkf <mywnajsldkf@gmail.com>
2022-01-02 23:25:51 +0900

    로그인 폼 제작
    - constraintLayout 사용
    - 폰트 이슈 발생(행간이 너무 넓음)-> 해결 필요

---
+++ Honeybee/app/src/main/java/com/example/honeybee/view/activity/AuthenticationActivity.java
@@ -0,0 +1,14 @@
+package com.example.honeybee.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
